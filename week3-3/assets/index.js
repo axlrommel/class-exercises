c8ea92384aceed1863e6e7b4b005e2e64fc0c3b0
axlrommel <rommelvillagomez77@gmail.com>
2024-10-29 12:09:42 -0500

    updates

--- week3-3/assets/index.js
+++ week3-3/assets/index.js
@@ -1,20 +1,7 @@
 var cardsHolder = document.getElementById('cardsPlaceholder');
+var itemList = [];
 
-var item = {
-  id: 1,
-  title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-  price: 109.95,
-  description:
-    'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-  category: "men's clothing",
-  image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-  rating: {
-    rate: 3.9,
-    count: 120,
-  },
-};
-
-function loadCards() {
+function loadCard(item) {
   var section = document.createElement('div');
   section.setAttribute('class', 'card');
   var title = document.createElement('h4');
@@ -23,10 +10,34 @@ function loadCards() {
   image.setAttribute('src', item.image);
   var price = document.createElement('p');
   price.textContent = item.price;
-
   section.appendChild(title);
   section.appendChild(image);
   section.appendChild(price);
-
   cardsHolder.appendChild(section);
 }
+
+function loadCards() {
+  fetch('https://fakestoreapi.com/products')
+    .then((response) => response.json())
+    .then((data) => {
+      itemList = data;
+      for (let i = 0; i < itemList.length; i++) {
+        loadCard(itemList[i]);
+      }
+    });
+}
+
+function sortAscending() {
+  itemList.sort((a, b) => a.price - b.price);
+  cardsHolder.replaceChildren();
+  for (let i = 0; i < itemList.length; i++) {
+    loadCard(itemList[i]);
+  }
+}
+
+
+
+
+
+
+
